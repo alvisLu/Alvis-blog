fdc9aa21068c0deba960b688b6d1055af31c39b9
Alvis Lu <uualvislu@gmail.com>
2024-07-27 14:03:31 +0800

    fix local

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -116,7 +116,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                 {tags && (
                   <div className="py-4 xl:py-8">
                     <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                      tags
+                      標籤
                     </h2>
                     <div className="flex flex-wrap">
                       {tags.map((tag) => (
